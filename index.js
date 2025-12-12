049a609c13fffdcd9e2f6a36cf9ecc9aeb127a21
Vaishnav <vaishnavpatil1143@gmail.com>
2025-12-12 19:15:04 +0530

    Changes

---
+++ index.js
@@ -0,0 +1,30 @@
+let todoInput = document.querySelector(".input");
+let addToDoButton = document.querySelector(".button");
+let showTodo = document.querySelector(".to-do-continer");
+let todo
+let todolist = [];
+
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+addToDoButton.addEventListener("click", (e)=>
+{
+    e.preventDefault();
+    todo = todoInput.value;
+    if(todo.length > 0)
+    {
+        todolist.push({id : uuid(), todo, isCompleted : false});
+    }
+    console.log(todolist);
+})
+
+function renderTodoList(todolist)
+{
+    showTodo.innerHTML = `<input type = "checkbox"> <label>Sky Diving</label> <button>Delete</button>`
+}
+renderTodoList(todolist);
